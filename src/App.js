6911191f101c7b43f602ccb73a26de294ed48d3a
Ndichu James <ndichujames2030@gmail.com>
2023-10-31 06:06:54 +0300

    done with the navbar section now its adding the different nav items

--- src/App.js
+++ src/App.js
@@ -2,11 +2,25 @@ import './App.css';
 import React from 'react';
 import { BrowserRouter as Router,Route, Routes } from "react-router-dom";
 import Home from './pages/home/home_screen';
-function app() {
+import Navbar from './pages/home/navbar';
+function App() {
   return (
-    <div className="app">
-      <Home />
+    <div className="App">
+      {/* Have our routes here */}
+      <Router>
+          <div>
+            <Navbar />
+            {/* Here i was specifying the different routes */}
+            <Routes>
+                {/* This is the only routes cause we only have one page in our application */}
+                <Route path="/" element={<Home/>}></Route>
+                {/* We have another routes incase you type your link wrongly */}
+                <Route path="*" element={<h1>404 Page Not Found</h1>}></Route>
+            </Routes>
+          </div>
+      </Router>
     </div>
   );
 }
-export default app;
+
+export default App;
